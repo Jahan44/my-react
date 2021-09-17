f6af7bc7a771b03f68fa3126b5c7625637c4b5ac
Jahan44 <joysenr96@gmail.com>
2021-09-17 20:35:37 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const product =[ 
+    {name:'Sumsung', Price:10000},
+    {name:'Samponi', Price:8000},
+    {name:'Vivo 91s', Price:7000}
+  ];
+
+  const [count,setCount]=useState(0);
+  const btnHandle =()=> setCount(count + 1);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Extra count={count}></Extra>
+        <Cart name={product[0].name} price={product[0].Price} btn={btnHandle}></Cart>
+        <Cart name={product[1].name} price={product[1].Price} btn={btnHandle}></Cart>
+        <Cart name={product[2].name} price={product[2].Price} btn={btnHandle}></Cart>
+     
       </header>
     </div>
   );
 }
 
+function Cart(popes){
+ 
+  const btnColor ={
+    borderRadius:'10px',
+    backgroundColor:'LightSeaGreen',
+    padding:'10px 15px',
+    color:'white'
+  };
+ 
+  return (
+    <div  >
+   
+    <div style={{width:'400px',color:'white',backgroundColor:'SlateGray',padding:'20px',borderRadius:'10px',marginTop:'50px'}}>
+      <h5 >Product Name: {popes.name}</h5>
+      <p >Product Price: {popes.price}</p>
+     
+      <button onClick={popes.btn} style={btnColor} >Go Buy</button>
+    </div>
+</div>
+  )
+}
+
+function Extra(popes){
+  return (
+   <div>
+     <h1 style={{marginBottom:'50px'}}> product count: {popes.count}</h1>
+   </div>
+  );
+}
 export default App;
